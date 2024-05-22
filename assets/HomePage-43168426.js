d70f5676d5f6c76e3216e7ee9070cded97076afe
AndriiSkliar <89527037+AndriiSkliar@users.noreply.github.com>
2024-05-22 09:58:32 +0000

    Deploying to gh-pages from @ AndriiSkliar/test_mgdevelopment@148c172735cea931e765811d6c4411f4379371eb 🚀

---
+++ assets/HomePage-43168426.js
@@ -0,0 +1,1 @@
+import{j as e}from"./index-d9022e9d.js";const r=()=>e.jsx("section",{children:e.jsx("h1",{children:"Home Page"})});export{r as default};
--- assets/HomePage-7eb69157.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-daae9488.js";const r=()=>e.jsx("section",{children:e.jsx("h1",{children:"Home Page"})});export{r as default};
