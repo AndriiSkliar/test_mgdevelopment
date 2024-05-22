fcdcc60f0b6428ac2c512ac750fb508b4127db15
Andrii Skliar <andreas.skliar@gmail.com>
2024-05-23 00:53:27 +0300

    squeezed sprite

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ export const Header = () => {
     <header className={css.header}>
       <nav className={css.nav}>
         <Link to="/home" className={css.logo}>
-          <img className="animate__animated animate__fadeInRightBig animate__delay-1s" src="/img/logo.jpg" alt="Mgdevelopment Logo" style={{ height: '40px' }} />
+          <img className="animate__animated animate__fadeInRightBig animate__delay-1s" src="img/logo.jpg" alt="Mgdevelopment Logo" style={{ height: '40px' }} />
           <span className={css.logoText}>Mgdevelopment</span>
         </Link>
         <NavLink to="/home" className={({ isActive }) => `${css.headerNavLink} ${isActive ? css.active : ''}`}>
